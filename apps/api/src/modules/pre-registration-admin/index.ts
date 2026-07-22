008dbb7886c2c4aea13289ad67214d109e86d93d
Claudinei Rogerio Gasparoto <crgasparoto@gmail.com>
2026-07-22 19:42:53 -0300

    feat(api): export pre-registration admin module (#270)

---
+++ apps/api/src/modules/pre-registration-admin/index.ts
@@ -0,0 +1,6 @@
+export { preRegistrationAdminRoutes } from './pre-registration-admin.routes.js';
+export {
+  PreRegistrationAdminError,
+  preRegistrationAdminService,
+  type PreRegistrationAdminActor,
+} from './pre-registration-admin.service.js';
