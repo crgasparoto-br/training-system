74996bf5d4f91d43e4b12f5f04211dfb3e6c1c52
Claudinei Rogerio Gasparoto <crgasparoto@gmail.com>
2026-07-23 09:30:35 -0300

    feat(api): export public pre-registration module

---
+++ apps/api/src/modules/pre-registration-public/index.ts
@@ -0,0 +1,8 @@
+export {
+  preRegistrationPublicEntryRoutes,
+  preRegistrationAuthenticatedRoutes,
+} from './pre-registration-public.routes.js';
+export {
+  PreRegistrationPublicError,
+  preRegistrationPublicService,
+} from './pre-registration-public.service.js';
