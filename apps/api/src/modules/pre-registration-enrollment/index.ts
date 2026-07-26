b3f4164b4e1f68aa623701ed50fd6fbe90118bb6
Claudinei Rogerio Gasparoto <crgasparoto@gmail.com>
2026-07-26 14:42:22 -0300

    feat(api): export enrollment conversion module (#274)

---
+++ apps/api/src/modules/pre-registration-enrollment/index.ts
@@ -0,0 +1,10 @@
+export {
+  preRegistrationEnrollmentRoutes,
+  preRegistrationPublicDeduplicationGuardRoutes,
+} from './pre-registration-enrollment.routes.js';
+export {
+  PreRegistrationEnrollmentError,
+  detectPreRegistrationDuplicates,
+  preRegistrationEnrollmentService,
+  type PreRegistrationEnrollmentActor,
+} from './pre-registration-enrollment.service.js';
