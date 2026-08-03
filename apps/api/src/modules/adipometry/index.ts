c12f0d2a43b7c4680bba0d595333acad483e4382
Claudinei Rogerio Gasparoto <crgasparoto@gmail.com>
2026-08-03 10:00:51 -0300

    feat(adpt): compor rotas operacionais e de apoio (#247)

--- apps/api/src/modules/adipometry/index.ts
+++ apps/api/src/modules/adipometry/index.ts
@@ -1,2 +1,11 @@
-export { default as adipometryRoutes } from './adipometry.routes.js';
+import { Router, type Router as ExpressRouter } from 'express';
+import adipometryOperationalRoutes from './adipometry.routes.js';
+import adipometryAnthropometrySupportRoutes from './adipometry-anthropometry-support.routes.js';
+
+const adipometryRoutes: ExpressRouter = Router();
+adipometryRoutes.use(adipometryOperationalRoutes);
+adipometryRoutes.use(adipometryAnthropometrySupportRoutes);
+
+export { adipometryRoutes };
 export * from './adipometry.service.js';
+export * from './adipometry-anthropometry-support.service.js';
